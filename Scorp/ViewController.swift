43d73e964a21a1cdf4e266d591c4e4843f5f29f5
Güney Köse <guney.kose@outlook.com>
2023-04-05 20:29:04 +0300

    Initial Commit

---
+++ Scorp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scorp
+//
+//  Created by Güney Köse on 5.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
